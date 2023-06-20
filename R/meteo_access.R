cd9ab887075a4c9240991c19c358fe809a067faa
K. Jennings <32177682+SnowHydrology@users.noreply.github.com>
2023-06-20 15:57:14 -0400

    update met access for lcd

--- R/meteo_access.R
+++ R/meteo_access.R
@@ -8,11 +8,13 @@ access_meteo <-
     # e.g. vars="TA"
     # access_meteo returns a specific set of vars per network
 
+    # Error handling if network not valid
     if(("ALL" %in% networks | "HADS" %in% networks | "LCD" %in% networks |
         "WCC" %in% networks) == FALSE) {
       stop("networks must be ALL, HADS, LCD, or WCC")
     }
 
+    # Error handling if time thresh too long or short
     if(time_thresh_s > 86400 | time_thresh_s < 600) {
       stop("time_thresh_s must be between 600 s and 86400 s")
     }
@@ -20,6 +22,9 @@ access_meteo <-
     # Compute the start and end times for downloading
     datetime_utc_start = datetime_utc_obs - time_thresh_s
     datetime_utc_end = datetime_utc_obs + time_thresh_s
+
+    # Create an empty df to store met data
+    met_all <- data.frame()
 
     # If ALL, add all networks
     if("ALL" %in% networks){
@@ -32,18 +37,30 @@ access_meteo <-
                                  deg_filter, dist_thresh_m)
       tmp_met <- download_meteo_hads(datetime_utc_start, datetime_utc_end, stations)
       tmp_met <- preprocess_meteo(network = "HADS", tmp_met)
+      met_all <- dplyr::bind_rows(met_all, tmp_met)
     }
 
     # Access LCD data
     if("LCD" %in% networks){
-
+      stations <- station_select(network = "LCD", lon_obs, lat_obs,
+                                 deg_filter, dist_thresh_m)
+      tmp_met <- download_meteo_lcd(datetime_utc_start, datetime_utc_end, stations,
+                                    lon_obs, lat_obs)
+      tmp_met <- preprocess_meteo(network = "LCD", tmp_met)
+      met_all <- dplyr::bind_rows(met_all, tmp_met)
     }
 
     # Access WCC data
     if("WCC" %in% networks){
-
+      stations <- station_select(network = "WCC", lon_obs, lat_obs,
+                                 deg_filter, dist_thresh_m)
+      tmp_met <- download_meteo_wcc(datetime_utc_start, datetime_utc_end, stations)
+      tmp_met <- preprocess_meteo(network = "WCC", tmp_met)
+      met_all <- dplyr::bind_rows(met_all, tmp_met)
     }
-    tmp_met
+
+    # Return the met_all data frame
+    met_all
 
   }
 
@@ -57,6 +74,11 @@ station_select <- function(network, lon_obs, lat_obs,
     # hads_meta <- read.csv("https://raw.githubusercontent.com/SnowHydrology/MountainRainOrSnow/main/data/metadata/hads_station_metadata_US.csv") %>%
     # dplyr::filter(substr(iem_network, start = 4, stop = 6) == "DCP")
     # usethis::use_data(hads_meta, internal = TRUE)
+  # LCD
+    # https://www.ncei.noaa.gov/maps/lcd/ (go to mapping tool, use polygon selector, drag over area of interest, and then click "Download Station List")
+    # Then use the mros_lcd_metadata_scrape.R script
+    # lcd_meta <- read.csv("https://raw.githubusercontent.com/SnowHydrology/MountainRainOrSnow/main/data/metadata/lcd_station_metadata_conus_withID.csv")
+    # usethis::use_data(hads_meta, lcd_meta, internal = TRUE, overwrite = TRUE)
 
   # Select stations from the HADS dataset
   if(network == "HADS"){
@@ -68,6 +90,30 @@ station_select <- function(network, lon_obs, lat_obs,
       dplyr::ungroup() %>%
       dplyr::filter(dist <= dist_thresh_m)
   }
+
+  # Select stations from the LCD dataset
+  if(network == "LCD"){
+    stations_tmp <- lcd_meta %>%
+      dplyr::filter(LONGITUDE >= lon_obs - deg_filter & LONGITUDE <= lon_obs + deg_filter,
+                    LATITUDE >= lat_obs - deg_filter & LATITUDE <= lat_obs + deg_filter) %>%
+      dplyr::rowwise() %>%
+      dplyr::mutate(dist = geosphere::distHaversine(c(lon_obs, lat_obs), c(LONGITUDE, LATITUDE))) %>%
+      dplyr::ungroup() %>%
+      dplyr::filter(dist <= dist_thresh_m)
+  }
+
+  # Select stations from the WCC dataset
+  if(network == "WCC"){
+    stations_tmp <- wcc_meta %>%
+      dplyr::filter(lon >= lon_obs - deg_filter & lon <= lon_obs + deg_filter,
+                    lat >= lat_obs - deg_filter & lat <= lat_obs + deg_filter) %>%
+      dplyr::rowwise() %>%
+      dplyr::mutate(dist = geosphere::distHaversine(c(lon_obs, lat_obs), c(lon, lat))) %>%
+      dplyr::ungroup() %>%
+      dplyr::filter(dist <= dist_thresh_m)
+  }
+
+  # Return the stations dataset
   stations_tmp
 }
 
@@ -181,25 +227,75 @@ download_meteo_hads <- function(datetime_utc_start, datetime_utc_end, stations){
   met
 }
 
-select_meteo <- function(network, tmp_met){
-  if(network == "HADS"){
+download_meteo_lcd <- function(datetime_utc_start, datetime_utc_end, stations,
+                               lon_obs, lat_obs){
 
-    # Identify columns and new names
-    lookup <- c(tair = "TAIRGZZ", tdew = "TDIRGZZ", rh = "XRIRGZZ")
+  # Specify the vars
+  # TODO: let user select vars
+  lcd_vars = "HourlyDewPointTemperature,HourlyDryBulbTemperature,HourlyPrecipitation,HourlyRelativeHumidity,HourlyWetBulbTemperature"
 
-    # Down-select columns and rename
-    tmp_met <- tmp_met %>%
-      dplyr::select(station, utc_valid, tidyselect::any_of(lookup))
+  # Figure out if there is more than 1 time zone to download
+  # NOAA serves LCD data in local standard time, NOT UTC
+  # Specifying UTC in the download string DOES NOT WORK - it still serves local time
+  tzs = unique(stations$timezone_lst)
+  n_tz = length(tzs)
 
-    # Compute number of columns and transform char data to numeric
-    n_cols = length(tmp_met)
-    tmp_met <- tmp_met %>%
-      dplyr::mutate(across(3:n_cols, as.numeric))
+  # Create a list of stations by timezone
+  stations_by_tz <- split(stations, stations$timezone_lst)
+
+  # Empty df to store data
+  met <- data.frame()
+
+  # Loop through n_tz
+  # Build URL
+  # And download data
+  for(i in 1:n_tz){
+
+    # Get start and end dates for tz
+    tmp_tz = stations_by_tz[[i]] %>%
+      dplyr::slice(1) %>%
+      dplyr::pull(timezone_lst)
+    datetime_lst_start = lubridate::with_tz(datetime_utc_start,
+                                            tzone = tmp_tz)
+    datetime_lst_end   = lubridate::with_tz(datetime_utc_end,
+                                            tzone = tmp_tz)
+
+    # Build URLS
+    lcd_url01_str = "https://www.ncei.noaa.gov/access/services/data/v1?dataset=local-climatological-data&stations="
+    lcd_url02_sta = paste(stations_by_tz[[i]]$id, collapse = ',')
+    lcd_url03_str = "&startDate="
+    lcd_url04_dat = format(datetime_lst_start, "%Y-%m-%dT%H:%M:%S")
+    lcd_url05_str = "&endDate="
+    lcd_url06_dat = format(datetime_lst_end, "%Y-%m-%dT%H:%M:%S")
+    lcd_url07_str = "&dataTypes="
+    lcd_url08_var = lcd_vars
+
+    url_lcd = paste0(lcd_url01_str, lcd_url02_sta, lcd_url03_str, lcd_url04_dat,
+                     lcd_url05_str, lcd_url06_dat, lcd_url07_str, lcd_url08_var)
+
+    # Download data
+    tmp_df <- read.csv(url_lcd)
+
+    # Format datetime
+    tmp_df <- tmp_df %>%
+      dplyr::mutate(datetime_lst = as.POSIXct(DATE,
+                                              format = "%Y-%m-%dT%H:%M:%S",
+                                              tz = tmp_tz),
+                    datetime = lubridate::with_tz(datetime_lst,
+                                                  tz = "UTC"))
+
+    # Bind data
+    met <- dplyr::bind_rows(met, tmp_df)
   }
+
+  # Return the data
+  met
 }
+
 
 preprocess_meteo <- function(network, tmp_met){
 
+  # Process HADS data
   if(network == "HADS"){
 
     # Identify columns and new names
@@ -228,5 +324,45 @@ preprocess_meteo <- function(network, tmp_met){
     tmp_met <- tmp_met %>%
       dplyr::mutate(dplyr::across(dplyr::contains('temp_'), ~ f_to_c(.)))
   }
+
+  # Process LCD data
+  if(network == "LCD"){
+    # Select only the appropriate columns and rename
+    # But first filter to only the FM-15 reports
+    tmp_met <- tmp_met %>%
+      dplyr::filter(REPORT_TYPE == "FM-15") %>%
+      dplyr::select(id = STATION, datetime,
+                    tair = HourlyDryBulbTemperature, tdew = HourlyDewPointTemperature,
+                    rh = HourlyRelativeHumidity, twet = HourlyWetBulbTemperature,
+                    ppt = HourlyPrecipitation)
+
+    # Convert any data points containing an "s" to NA
+    # These don't pass the stations QC checks
+    tmp_met <- tmp_met %>%
+      dplyr::mutate(
+        tair2 = dplyr::case_when(grepl("s", tair) ~ NA_real_,
+                          TRUE ~ as.numeric(as.character(tair))),
+        twet2 = dplyr::case_when(grepl("s", twet) ~ NA_real_,
+                          TRUE ~ as.numeric(as.character(twet))),
+        tdew2 = dplyr::case_when(grepl("s", tdew) ~ NA_real_,
+                          TRUE ~ as.numeric(as.character(tdew))),
+        ppt2  = dplyr::case_when(grepl("s", ppt) ~ NA_real_,
+                          TRUE ~ as.numeric(as.character(ppt))),
+        rh2   = dplyr::case_when(grepl("s", rh) ~ NA_real_,
+                          TRUE ~ as.numeric(as.character(rh)))
+      ) %>%
+      dplyr::mutate(ppt2 = ifelse(ppt == "T", 0.0001, ppt2))
+
+    # Convert temp to celsius and ppt to mm
+    tmp_met <- tmp_met %>%
+      dplyr::select(id, datetime,
+             temp_air = tair2, temp_dew = tdew2, temp_wet = twet2, rh = rh2, ppt = ppt2) %>%
+      dplyr::mutate(temp_air = f_to_c(temp_air),
+                    temp_dew = f_to_c(temp_dew),
+                    temp_wet = f_to_c(temp_wet),
+                    ppt = in_to_mm(ppt))
+  }
+
+  # Return the data
   tmp_met
 }
