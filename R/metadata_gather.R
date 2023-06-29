6a3785e91c5e7ccb6c8b4bcabad17ef2b54b2315
K. Jennings <32177682+SnowHydrology@users.noreply.github.com>
2023-06-29 14:17:15 -0400

    gather metadata using new function

---
+++ R/metadata_gather.R
@@ -0,0 +1,14 @@
+#' Function to gather metadata from meta_all using a vector of station ids
+#'
+#' @param stations Vector of station ids
+#'
+#' @return A dataframe of metadata filter to the station ids in 'stations'
+#' @export
+#'
+#' @examples
+#' stations = c("RRMI4", "INLO2", "JVDU1", "72526604751", "RDKC2", "TABC1", "LSUA2", "BAFO3", "SOAC1", "BNTC1")
+#' gather_meta(stations)
+gather_meta <- function(stations){
+  # filter the metadata to only those in the station list
+  dplyr::filter(all_meta, id %in% stations)
+}
